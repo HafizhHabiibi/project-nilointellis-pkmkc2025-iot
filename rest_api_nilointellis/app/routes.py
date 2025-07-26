bccf2fcad5a17ab99443b0f5cdcb7dcde2766d99
HafizhHabiibi <hafizhhabiibi123@gmail.com>
2025-07-26 21:33:56 +0700

    fix: timezone sensor history

--- rest_api_nilointellis/app/routes.py
+++ rest_api_nilointellis/app/routes.py
@@ -26,6 +26,8 @@ data_terakhir = {}
 def konversi_wib(dt_utc):
     if not isinstance(dt_utc, datetime):
         return dt_utc
+    if dt_utc.tzinfo is None:
+        dt_utc = dt_utc.replace(tzinfo=timezone.utc)
     wib = pytz.timezone("Asia/Jakarta")
     return dt_utc.astimezone(wib).strftime('%Y-%m-%d %H:%M:%S')
 
