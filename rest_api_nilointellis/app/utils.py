de77b3eedd43ca13a3de15ca7967a78c9db85f86
HafizhHabiibi <hafizhhabiibi123@gmail.com>
2025-07-29 08:01:22 +0700

    fix: typo url

--- rest_api_nilointellis/app/utils.py
+++ rest_api_nilointellis/app/utils.py
@@ -4,7 +4,7 @@ from flask import current_app
 def send_notif(message):
     token = current_app.config['TELEGRAM_BOT_TOKEN']
     chat_id = current_app.config['CHAT_ID']
-    url = f"https://api.telegram.org/bot8{token}/sendMessage"
+    url = f"https://api.telegram.org/bot{token}/sendMessage"
 
     payload = {
         'chat_id': chat_id,
