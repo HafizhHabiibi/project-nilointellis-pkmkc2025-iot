029e9483f10bfe1322ac28a670cff6fd111f80fc
HafizhHabiibi <hafizhhabiibi123@gmail.com>
2025-07-29 07:14:20 +0700

    feat: fungsi send notif to tele

---
+++ rest_api_nilointellis/app/utils.py
@@ -0,0 +1,17 @@
+import requests
+from flask import current_app
+
+def send_notif(message):
+    token = current_app.config['TELEGRAM_BOT_TOKEN']
+    chat_id = current_app.config['CHAT_ID']
+    url = f"https://api.telegram.org/bot8418026355:AAEGCHdwqeq9tm-tO2WMKWCU5NLgORoZtXM/sendMessage"
+
+    payload = {
+        'chat_id': chat_id,
+        'text' : message,
+    }
+
+    try:
+        requests.post(url, data=payload)
+    except Exception as e:
+        print("Gagal kirim ke Telegram", e)
