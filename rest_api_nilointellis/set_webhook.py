3f1d88f0286d8446dacf5fdc16bface1e2faaa29
HafizhHabiibi <hafizhhabiibi123@gmail.com>
2025-09-20 14:58:20 +0700

    fix(set_webhook): clear code set_webhook

--- rest_api_nilointellis/set_webhook.py
+++ rest_api_nilointellis/set_webhook.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 token = os.getenv("TELEGRAM_BOT_TOKEN")
-ngrok_url = "https://8e9f25b3c974.ngrok-free.app"  # ganti dengan URL dari ngrok
+ngrok_url = ""  # ganti dengan URL dari ngrok
 
 url = f"https://api.telegram.org/bot{token}/setWebhook?url={ngrok_url}/telegram"
 resp = requests.get(url)
