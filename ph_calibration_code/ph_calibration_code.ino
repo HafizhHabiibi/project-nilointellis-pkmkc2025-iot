c5422894f2e5295f4e4a63b78b8cf02095b4d510
HafizhHabiibi <hafizhhabiibi123@gmail.com>
2025-09-21 23:14:52 +0700

    fix(ph_cal_code): Ubah library default df robot ke library modifikasi

--- ph_calibration_code/ph_calibration_code.ino
+++ ph_calibration_code/ph_calibration_code.ino
@@ -1,51 +1,70 @@
-#include "DFRobot_PH.h"
-#include <EEPROM.h>
-#include <OneWire.h>
-#include <DallasTemperature.h>
+/*
+ * file DFRobot_ESP_PH_BY_GREENPONIK.ino
+ * @ https://github.com/GreenPonik/DFRobot_ESP_PH_BY_GREENPONIK
+ *
+ * This is the sample code for Gravity: Analog pH Sensor / Meter Kit V2, SKU:SEN0161-V2
+ * In order to guarantee precision, a temperature sensor such as DS18B20 is needed, to execute automatic temperature compensation.
+ * You can send commands in the serial monitor to execute the calibration.
+ * Serial Commands:
+ *   enterph -> enter the calibration mode
+ *   calph   -> calibrate with the standard buffer solution, two buffer solutions(4.0 and 7.0) will be automaticlly recognized
+ *   exitph  -> save the calibrated parameters and exit from calibration mode
+ * 
+ * Based on the @ https://github.com/DFRobot/DFRobot_PH
+ * Copyright   [DFRobot](http://www.dfrobot.com), 2018
+ * Copyright   GNU Lesser General Public License
+ *
+ * ##################################################
+ * ##################################################
+ * ########## Fork on github by GreenPonik ##########
+ * ############# ONLY ESP COMPATIBLE ################
+ * ##################################################
+ * ##################################################
+ * 
+ * version  V1.1
+ * date  2019-06
+ */
 
+#include "DFRobot_ESP_PH.h"
+#include "EEPROM.h"
 
-#define PH_PIN 34       
-#define ONE_WIRE_BUS 13   
+DFRobot_ESP_PH ph;
+#define ESPADC 4096.0   //the esp Analog Digital Convertion value
+#define ESPVOLTAGE 3300 //the esp voltage supply value
+#define PH_PIN 32		//the esp gpio data pin number
+float voltage, phValue, temperature = 25;
 
-// --- Objek sensor ---
-DFRobot_PH ph;
-OneWire oneWire(ONE_WIRE_BUS);
-DallasTemperature sensors(&oneWire);
-
-float voltage;
-float phValue;
-float temperature;
-
-void setup() {
-  Serial.begin(115200);
-
-  sensors.begin();  
-  ph.begin();       // inisialisasi pH sensor (load kalibrasi dari EEPROM)
+void setup()
+{
+	Serial.begin(115200);
+	EEPROM.begin(32);//needed to permit storage of calibration value in eeprom
+	ph.begin();
 }
 
-void loop() {
-  static unsigned long timepoint = millis();
-  if (millis() - timepoint > 1000U) {   
-    timepoint = millis();
+void loop()
+{
+	static unsigned long timepoint = millis();
+	if (millis() - timepoint > 1000U) //time interval: 1s
+	{
+		timepoint = millis();
+		//voltage = rawPinValue / esp32ADC * esp32Vin
+		voltage = analogRead(PH_PIN) / ESPADC * ESPVOLTAGE; // read the voltage
+		Serial.print("voltage:");
+		Serial.println(voltage, 4);
+		
+		//temperature = readTemperature();  // read your temperature sensor to execute temperature compensation
+		Serial.print("temperature:");
+		Serial.print(temperature, 1);
+		Serial.println("^C");
 
-    // --- Baca suhu dari DS18B20 ---
-    sensors.requestTemperatures();
-    temperature = sensors.getTempCByIndex(0);
+		phValue = ph.readPH(voltage, temperature); // convert voltage to pH with temperature compensation
+		Serial.print("pH:");
+		Serial.println(phValue, 4);
+	}
+	ph.calibration(voltage, temperature); // calibration process by Serial CMD
+}
 
-    // --- Baca tegangan dari sensor pH ---
-    int adcValue = analogRead(PH_PIN);                
-    voltage = (adcValue / 4095.0) * 3300;             
-
-    // --- Hitung nilai pH dengan kompensasi suhu ---
-    phValue = ph.readPH(voltage, temperature);
-
-    // --- Tampilkan ke Serial Monitor ---
-    Serial.print("Temperature: ");
-    Serial.print(temperature, 1);
-    Serial.print(" °C   pH: ");
-    Serial.println(phValue, 2);
-  }
-
-  // --- Proses kalibrasi lewat Serial Monitor ---
-  ph.calibration(voltage, temperature);
+float readTemperature()
+{
+	//add your code here to get the temperature from your temperature sensor
 }
